9fbb837549afa328c34564638b4cbcf600930ea6
Manu Hegde <manu.hegde02@gmail.com>
2024-03-31 18:48:43 -0400

    Added nav bar and the cards for team members, along with JS component of shuffling images

---
+++ src/docs/milestone-01/script.js
@@ -0,0 +1,19 @@
+const pictures = {"victor": ["images/baby_victor.jpg", "images/mid_victor.jpg", "images/old_victor.jpg"], 
+"aarav": ["images/Baby_Aarav.JPG", "images/eyes_aarav.jpg", "images/Modern_Aarav.JPG"], 
+"vin": ["images/Baby_Vin.jpg", "images/Mid_Vin.jpg", "images/Bio_Image_Vin_1.jpg"]}
+
+function shufflePictures(person) {
+    const card = document.getElementById(person + "-picture");
+    const img = card.children[0];
+    let index = -1
+    for (let image of pictures[person]) {
+        if (img.src.includes(image)) index = pictures[person].indexOf(image);
+    }
+    img.src = pictures[person][(index + 1) % 3];
+}
+
+
+const cards = document.getElementsByClassName("card");
+for (const card of cards) {
+    card.addEventListener("click", () => shufflePictures(card.id));
+}
