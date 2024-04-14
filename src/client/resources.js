82ee892af9d338704cafb89976b45e7bf1001ee6
Manu Hegde <manu.hegde02@gmail.com>
2024-04-14 17:13:03 -0400

    Added cards for resources page

---
+++ src/client/resources.js
@@ -0,0 +1,11 @@
+function displayTooltip() {
+    tooltip = document.get
+}
+
+
+function close(id) {
+    const tooltip = document.getElementById(id);
+    tooltip.style.display = hidden;
+    const text = tooltip.children[0];
+    text.style.display = hidden;
+}
