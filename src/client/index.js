e5229581c994405dad3989d5daf98b0f4be7dfa3
Manu Hegde <manu.hegde02@gmail.com>
2024-04-17 14:04:02 -0400

    Added navbar for views and tooltips for resources section

--- src/client/index.js
+++ src/client/index.js
@@ -9,3 +9,35 @@ function close(id) {
     const text = tooltip.children[0];
     text.style.display = hidden;
 }
+
+
+function sectionDisplay(name) {
+    // To hide all other sections when using navbar
+    sections = document.getElementsByClassName("main-sections");
+    for (s of sections) {
+        if (s.id != name) s.style.display = none;
+        else s.style.removeProperty("display");
+    }
+}
+
+function tooltipDisplay(name) {
+    const tooltip = document.getElementById(name + "-tooltip")
+    const text = document.getElementById(name + "-tooltip-text")
+    tooltip.style.visibility = tooltip.style.visibility === "visible" ? "hidden" : "visible";
+    tooltip.style.opacity = 1 - tooltip.style.opacity;
+    text.style.visibility = tooltip.style.visibility;
+    text.style.opacity = tooltip.style.opacity;
+}
+
+
+document.getElementById("mapBtn").addEventListener("click", () => sectionDisplay("map-section"));
+document.getElementById("simBtn").addEventListener("click", () => sectionDisplay("simulation-section"));
+document.getElementById("resourcesBtn").addEventListener("click", () => sectionDisplay("resources-section"));
+document.getElementById("uviewBtn").addEventListener("click", () => sectionDisplay("uview-section"));
+
+cardPictures = document.getElementsByClassName("card-picture");
+for (p of cardPictures) {
+    const name = p.id;
+    p.addEventListener("mouseover", () => tooltipDisplay(name));
+    p.addEventListener("mouseout", () => tooltipDisplay(name));
+}
