cad7a0f35e17d0a89487fd7ab1f42bbf8339b9b7
VinT04 <tyagivinamra@gmail.com>
2024-04-23 17:14:02 -0400

    Changes to the structure of the resources, adding tooltips in the center of the Earth image in the middle as well as other minor changes to the layout of the icons.

--- src/client/index.js
+++ src/client/index.js
@@ -1,43 +1,88 @@
-function displayTooltip() {
-    tooltip = document.get
-}
-
-
-function close(id) {
-    const tooltip = document.getElementById(id);
-    tooltip.style.display = hidden;
-    const text = tooltip.children[0];
-    text.style.display = hidden;
-}
-
-
 function sectionDisplay(name) {
-    // To hide all other sections when using navbar
-    sections = document.getElementsByClassName("main-sections");
-    for (s of sections) {
-        if (s.id != name) s.style.display = none;
-        else s.style.removeProperty("display");
-    }
+  // To hide all other sections when using navbar
+  sections = document.getElementsByClassName("main-sections");
+  for (s of sections) {
+    if (s.id != name) s.style.display = none;
+    else s.style.removeProperty("display");
+  }
 }
 
 function tooltipDisplay(name) {
-    const tooltip = document.getElementById(name + "-tooltip")
-    const text = document.getElementById(name + "-tooltip-text")
-    tooltip.style.visibility = tooltip.style.visibility === "visible" ? "hidden" : "visible";
-    tooltip.style.opacity = 1 - tooltip.style.opacity;
-    text.style.visibility = tooltip.style.visibility;
-    text.style.opacity = tooltip.style.opacity;
+  const tipElement = document.getElementById("tip");
+  switch (name) {
+    case "UNFCCC": {
+      tipElement.innerText =
+        "The UN process for negotiating\n an agreement to limit dangerous\n climate change.";
+      break;
+    }
+    case "IPCC": {
+      tipElement.innerText =
+        "An intergovernmental body of the United Nations made to\n advance scientific knowledge\n about climate change.";
+      break;
+    }
+    case "WMO": {
+      tipElement.innerText =
+        "Agency of the United Nations\n responsible for promoting\n international cooperation on\n atmospheric science.";
+      break;
+    }
+    case "World-Bank": {
+      tipElement.innerText =
+        "The World Bank Group is the\n biggest multilateral funder\n of climate investments in\n developing countries.";
+      break;
+    }
+    case "UNEP": {
+      tipElement.innerText =
+        "responsible for coordinating\n responses to environmental issues\n within the United Nations system.";
+      break;
+    }
+    case "GCF": {
+      tipElement.innerText =
+        "Accelerates transformative\n climate action in\n developing countries";
+      break;
+    }
+    case "CAN": {
+      tipElement.innerText =
+        "A global network of over 1,300\n environmental non-governmental\n organisations";
+      break;
+    }
+    case "Climate-Group": {
+      tipElement.innerText =
+        "A non-profit organisation that\n works with businesses and\n government leaders aiming\n to address climate change. ";
+      break;
+    }
+    case "GGGI": {
+      tipElement.innerText =
+        "Intergovernmental organization\n based in Seoul, South Korea.\n It is dedicated to promoting\n green growth around\n the world.";
+      break;
+    }
+    case "IRENA": {
+      tipElement.innerText =
+        "intergovernmental organization\n mandated to facilitate\n cooperation of renewable energy.";
+      break;
+    }
+  }
 }
 
+function clearTooltipDisplay() {
+  document.getElementById("tip").innerText = "";
+}
 
-document.getElementById("mapBtn").addEventListener("click", () => sectionDisplay("map-section"));
-document.getElementById("simBtn").addEventListener("click", () => sectionDisplay("simulation-section"));
-document.getElementById("resourcesBtn").addEventListener("click", () => sectionDisplay("resources-section"));
-document.getElementById("uviewBtn").addEventListener("click", () => sectionDisplay("uview-section"));
+document
+  .getElementById("mapBtn")
+  .addEventListener("click", () => sectionDisplay("map-section"));
+document
+  .getElementById("simBtn")
+  .addEventListener("click", () => sectionDisplay("simulation-section"));
+document
+  .getElementById("resourcesBtn")
+  .addEventListener("click", () => sectionDisplay("resources-section"));
+document
+  .getElementById("uviewBtn")
+  .addEventListener("click", () => sectionDisplay("uview-section"));
 
 cardPictures = document.getElementsByClassName("card-picture");
 for (p of cardPictures) {
-    const name = p.id;
-    p.addEventListener("mouseover", () => tooltipDisplay(name));
-    p.addEventListener("mouseout", () => tooltipDisplay(name));
+  const name = p.id;
+  p.addEventListener("mouseover", () => tooltipDisplay(name));
+  p.addEventListener("mouseout", () => clearTooltipDisplay(name));
 }
