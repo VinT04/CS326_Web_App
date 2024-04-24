bb282f2679fb566c111d987bac2a8baf2dda6cd9
aarav-nair <aarav.nair@gmail.com>
2024-04-23 21:55:01 -0400

    Cleanup

--- src/client/index.js
+++ src/client/index.js
@@ -96,7 +96,7 @@ function createSimulationChart() {
 
     const initialData = [49.5, 50, 50.5, 51, 51.5, 52, 52.5, 53, 53.5, 54, 54.5, 55, 55.5, 56, 56.5, 57];
     const xValues = [2025, 2030, 2035, 2040, 2045, 2050, 2055, 2060, 2065, 2070, 2075, 2080, 2085, 2090, 2095, 2100];
-    const yValues = initialData.map(data => data + 0.05 * electricityChange + 0.03 * transportationChange + 0.05 * agricultureChange + 0.04 * industryChange + 0.03 * otherChange);
+    const yValues = initialData.map(x => x + 0.05 * electricityChange + 0.03 * transportationChange + 0.05 * agricultureChange + 0.04 * industryChange + 0.03 * otherChange);
     const projColor = yValues[0] > initialData[0] ? "red" : "green";
 
     new Chart('predictionChart', {
@@ -118,7 +118,11 @@ function createSimulationChart() {
         },       
         options: {
             scales: {
-              yAxes: [{ticks: {min: 20, max:80, stepSize: 5}}],
+              yAxes: [{ticks: {
+                min: 20, 
+                max:80, 
+                stepSize: 5
+            }}],
             }
           }
     });
