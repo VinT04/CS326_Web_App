3b208dbca96cec23934c9d4200de6f4762378bed
aarav-nair <aarav.nair@gmail.com>
2024-04-23 21:51:57 -0400

    Stacked graph

--- src/client/index.js
+++ src/client/index.js
@@ -94,19 +94,26 @@ function createSimulationChart() {
     const industryChange = parseFloat(document.getElementById("industryRangeValue").innerText);
     const otherChange = parseFloat(document.getElementById("otherRangeValue").innerText);
 
-    const barColor = "green";
     const initialData = [49.5, 50, 50.5, 51, 51.5, 52, 52.5, 53, 53.5, 54, 54.5, 55, 55.5, 56, 56.5, 57];
     const xValues = [2025, 2030, 2035, 2040, 2045, 2050, 2055, 2060, 2065, 2070, 2075, 2080, 2085, 2090, 2095, 2100];
     const yValues = initialData.map(data => data + 0.05 * electricityChange + 0.03 * transportationChange + 0.05 * agricultureChange + 0.04 * industryChange + 0.03 * otherChange);
+    const projColor = yValues[0] > initialData[0] ? "red" : "green";
 
     new Chart('predictionChart', {
         type: 'bar',
         data: {
             labels: xValues,
             datasets: [{
-                label: 'Temperature',
-                backgroundColor: barColor,
-                data: yValues
+                label: 'Current Temperature',
+                backgroundColor: "yellow",
+                data: initialData,
+                stack: 'Stack 0'
+            }, 
+            {
+                label: 'Projected Temperature',
+                backgroundColor: projColor,
+                data: yValues,
+                stack: 'Stack 1'
             }]
         },       
         options: {
