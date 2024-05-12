d2b186388c91ab200bcbce9a737043d7520b6191
vc64 <vchen2022@spyponders.com>
2024-05-12 13:52:14 -0400

    integrated real data into map view

--- src/client/index.js
+++ src/client/index.js
@@ -1,4 +1,4 @@
-import { response } from "express";
+// import { response } from "express";
 
 const URL = "http://localhost:3260";
 
@@ -182,7 +182,7 @@ async function getLocation() {
       longitude = 42.3732;
     };
     navigator.geolocation.getCurrentPosition(successHandler, errorHandler);
-    url = "http://api.geonames.org/countryCodeJSON?lat=" + latitude + "&lng=" + longitude + "&username=bionic";
+    const url = "http://api.geonames.org/countryCodeJSON?lat=" + latitude + "&lng=" + longitude + "&username=bionic";
     
     const response = await fetch(url);
     if (response.ok) {
@@ -297,14 +297,14 @@ function initColorBtns() {
   }
 }
 
-// stores all data, with keys being numberic IDs corresponding to each data file (see dataNameKey)
-const data = {};
+// // stores all data, with keys being numberic IDs corresponding to each data file (see dataNameKey)
+// const data = {};
 
 // stores the domain (min, max) for each data file
 const domainKey = {};
 
-// stores mapping between data names and numeric IDs
-const dataNameKey = {"Data 1 (ex: CO2)":1, "Data 2 (ex: Avg Temp)":2, "Data 3 (ex: Avg Rainfall)":3, "Data 4 (ex: Deforestation)":4}
+// stores mapping between numeric IDs and data names
+const dataNameKey = {};
 
 /**
  * Initializes the data and domainKey objects by reading data files from /data.
@@ -313,29 +313,119 @@ const dataNameKey = {"Data 1 (ex: CO2)":1, "Data 2 (ex: Avg Temp)":2, "Data 3 (e
  * data object.
  */
 async function initData() {
-  let i = 1;
-  // iterate through all data IDs
-  while (i <= 4) {
-    data[i] = await d3.csv(`data/mock_data${i}.csv`)
-      .then(rawData => {
-        // obtain min and max values in the data, as well as a parsed version of the data
-        // that maps country ISO3 to the desired data values
-        const [min, max, parsedData] = rawData.reduce((acc, elem) => {
-          acc[2][elem["ISO3"]] = elem;
-          const [currMin, currMax] = Object.entries(elem).reduce((domain, keyVal) => {
-            // only want to look at values which have a year as a key
-            if (isNaN(keyVal[0])) {
-              return domain;
+  const data_files = [
+    {
+      filename: "avg_temp_change", source: "Food and Agriculture Organization Corporate Statistical Database", 
+      units: "°C", title: "Average Temperature Change since 1951-1980", label: "Temperature Change"
+    },
+    {
+      filename:"ghg-emissions", source: "Climate Watch",
+      units: "MtCO2e per person", title: "Carbon Dioxide Emissions Per Capita", label: "CO2 Emissions"
+    },
+    {
+      filename:"Forest_and_Carbon", source: "Food and Agriculture Organization of the United Nations (FAO)",
+      units: "%", title: "Forest Cover Index (relative to 1992)", label: "Forest Cover"
+    },
+    {
+      filename:"Climate-related_Disasters_Frequency", source: "The Emergency Events Database (EM-DAT)", units: "disasters",
+      title: "Frequency of Climate-related Disasters (Wildfires, Droughts, Floods, etc.)", label: "Climate-related Disasters"},
+  ];
+  let population_data = await d3.csv(`data/population.csv`);
+  const object_index = {};
+  population_data.forEach(row => {
+    if (!(row["ISO3"] in object_index)) {
+      object_index[row["ISO3"]] = {};
+    }
+    object_index[row["ISO3"]][row["Year"]] = row["Population (historical estimates)"];
+  });
+  population_data = object_index;
+  return Promise.all(data_files.map(async (fileInfo, idx) => {
+    let rawData = await d3.csv(`data/${fileInfo.filename}.csv`);
+    rawData = rawData.filter(row => {
+      if (fileInfo.filename == "Forest_and_Carbon") {
+        if (row["Indicator"] === "Index of forest extent" && row["ISO3"].length === 3) {
+          return true;
+        }
+        return false;
+      }
+      else if (fileInfo.filename === "Climate-related_Disasters_Frequency") {
+        if (row["Indicator"] === "Climate related disasters frequency, Number of Disasters: TOTAL") {
+          return true;
+        }
+        return false;
+      }
+      return true;
+    });
+    if (fileInfo.filename === "ghg-emissions") {
+      rawData.map(row => {
+        if (row["ISO3"].length === 3) {
+          for (const key in row) {
+            if (!isNaN(key) && !isNaN(row[key])) {
+              row[key] = (parseFloat(row[key])*1000000/parseFloat(population_data[row["ISO3"]][key])).toFixed(3);
             }
-            return [Math.min(domain[0], +keyVal[1]), Math.max(domain[1], +keyVal[1])];
-          }, [Infinity, -Infinity]);
-          return [currMin, currMax, acc[2]];
-        }, [Infinity, -Infinity, {}]);
-        domainKey[i] = {min: Math.floor(min), max: Math.ceil(max)};
-        return parsedData;
+          }
+        }
       });
-    i++;
-  }
+    }
+    let [minYear, maxYear] = [Infinity, -Infinity];
+    const [min, max, parsedData] = rawData.reduce((acc, elem) => {
+      acc[2][elem["ISO3"]] = elem;
+      const [currMin, currMax] = Object.entries(elem).reduce((domain, keyVal) => {
+        // only want to look at values which have a year as a key
+        if (isNaN(keyVal[0]) || keyVal[1] === '') {
+          return domain;
+        }
+        minYear = Math.min(minYear, keyVal[0]);
+        maxYear = Math.max(maxYear, keyVal[0]);
+        if (+keyVal[1] === 0) {
+          console.log(keyVal);
+        }
+        return [Math.min(domain[0], +keyVal[1]), Math.max(domain[1], +keyVal[1])];
+      }, [acc[0], acc[1]]);
+      return [currMin, currMax, acc[2]];
+    }, [Infinity, -Infinity, {}]);
+    parsedData["domain"] = {min: Math.floor(min), max: Math.ceil(max)};
+    parsedData["yearRange"] = {min: minYear, max: maxYear};
+    parsedData["source"] = fileInfo.source;
+    parsedData['units'] = fileInfo.units;
+    parsedData['title'] = fileInfo.title;
+    parsedData["label"] = fileInfo.label;
+    return parsedData;
+  }))
+
+  // // iterate through all data CSVs
+  // while (i <= 4) {
+  //   data[i] = await d3.csv(`data/mock_data${i}.csv`)
+  //     .then(rawData => {
+  //       // obtain min and max values in the data, as well as a parsed version of the data
+  //       // that maps country ISO3 to the desired data values
+  //       const [min, max, parsedData] = rawData.reduce((acc, elem) => {
+  //         acc[2][elem["ISO3"]] = elem;
+  //         const [currMin, currMax] = Object.entries(elem).reduce((domain, keyVal) => {
+  //           // only want to look at values which have a year as a key
+  //           if (isNaN(keyVal[0])) {
+  //             return domain;
+  //           }
+  //           return [Math.min(domain[0], +keyVal[1]), Math.max(domain[1], +keyVal[1])];
+  //         }, [Infinity, -Infinity]);
+  //         return [currMin, currMax, acc[2]];
+  //       }, [Infinity, -Infinity, {}]);
+  //       domainKey[i] = {min: Math.floor(min), max: Math.ceil(max)};
+  //       return parsedData;
+  //     });
+  //   i++;
+  // }
+  // console.log(data);
+}
+
+/**
+ * Loads processed data from JSON file. Data was first processed with initData(),
+ * then stored as a JSON file to skip data processing for future. Data object loaded in stores an array
+ * of data objects, each containing values per country measuring a particular metric/type of data.
+ * Also updates the dataNameKey.
+ */
+async function loadMapData() {
+
 }
 
 /**
@@ -356,9 +446,9 @@ function initMapSlider() {
     updateAnimateBtn();
     // when button is clicked, automatically increment year and update the side panel and text
     if (btnState === 1) {
-      slider.value = slider.value % 2022;
+      slider.value = slider.value % slider.max;
       sliderUpdates = setInterval(() => {
-        if (slider.value === "2022") {
+        if (slider.value === slider.max) {
           updateAnimateBtn();
           clearInterval(sliderUpdates);
         } else {
@@ -372,6 +462,10 @@ function initMapSlider() {
       clearInterval(sliderUpdates);
     }
   }
+  slider.min = data[currentData].yearRange.min;
+  slider.max = data[currentData].yearRange.max;
+  slider.value = data[currentData].yearRange.min;
+  year.innerText = slider.value;
 }
 
 /**
@@ -423,7 +517,7 @@ function makeMap(topology) {
   .attr("fill", d => {
     const countryObj = countryCodes[d.id] || {}
     const countryData = data[currentData][countryObj["alpha-3"]] || {};
-    return colorScale(countryData[sliderYear] || 0);
+    return colorScale(countryData[sliderYear] || data[currentData].domain.min);
   })
   .on("mouseover", mouseOver)
   .on("mouseleave", mouseLeave)
@@ -446,9 +540,10 @@ function mouseOver(event) {
       .style("stroke-width", "0.25px")
   const countryID = event.srcElement.__data__.id;
   const countryObj = countryCodes[countryID] || {};
-  const countryData = data[currentData][countryObj["alpha-3"]] || {};
+  currCountry = countryObj["alpha-3"];
+  const countryData = data[currentData][currCountry] || {};
   country.innerText = countryObj["name"] || "N/A";
-  currData.innerText = countryData[sliderYear] ? `${countryData[sliderYear]} ${dataKey[currentData].units}` : "No data available";
+  currData.innerText = countryData[sliderYear] ? `${countryData[sliderYear]} ${data[currentData].units}` : "No data available";
 }
 /**
  * Returns the country highlighted to normal hue and reverts the data display
@@ -465,6 +560,7 @@ function mouseLeave() {
       .style("stroke-width", "0.1px");
   country.innerText = "None";
   currData.innerText = "None";
+  currCountry = "";
 }
 
 /**
@@ -472,8 +568,9 @@ function mouseLeave() {
  * @returns A d3 color scale object corresponding to the current color scheme.
  */
 function currColorScale() {
+  // console.log(data[currentData]["domain"])
   return d3.scaleSequential()
-      .domain([domainKey[currentData].min, domainKey[currentData].max])
+      .domain([data[currentData]["domain"]["min"], data[currentData]["domain"]["max"]])
       .interpolator(scaleKey[currentColor]);
 }
 const updateColorScale = () => colorScale = currColorScale();
@@ -483,14 +580,20 @@ const updateColorScale = () => colorScale = currColorScale();
  * statistics
  */
 function updateMap() {
+  if (currCountry === "") {
+    currData.innerText = "None";
+  } else {
+    const currInfo = (data[currentData][currCountry] || {})[sliderYear];
+    currData.innerText = currInfo ? `${currInfo} ${data[currentData].units}` : "No data available";
+  }
   d3.selectAll("path")
     .transition()
     .duration(75)
     .attr("fill", d => {
       const countryObj = countryCodes[d.id] || {};
-      const currData = data[currentData][countryObj["alpha-3"]] || {};
+      const selectedData = data[currentData][countryObj["alpha-3"]] || {};
       updateColorScale();
-      return colorScale(currData[sliderYear] || 0);
+      return colorScale(selectedData[sliderYear] || data[currentData].domain.min);
     });
 }
 
@@ -504,6 +607,9 @@ function initDropdown() {
   const dropdownMenu = document.getElementById("data-menu")
   const dropdownOptions = document.querySelectorAll("#data-menu li");
 
+  title.innerText = data[currentData].title;
+  dropdownSelected.innerText = data[currentData].label;
+
   dropdownSelection.addEventListener("click", () => {
     dropdownSelection.classList.toggle("clicked");
     dropdownArrow.classList.toggle("rotated");
@@ -511,6 +617,7 @@ function initDropdown() {
   });
 
   dropdownOptions.forEach(option => {
+    option.innerText = data[option.getAttribute("data-id")]["label"];
     option.addEventListener("click", () => {
       dropdownSelected.innerText = option.innerText;
       dropdownSelection.classList.remove("clicked");
@@ -520,16 +627,21 @@ function initDropdown() {
         otherOption.classList.remove("active");
       });
       option.classList.toggle("active");
-      currentData = dataNameKey[option.innerText];
-      dataName.innerText = `${dataKey[currentData].label}:`;
-      title.innerText = dataKey[currentData].title;
+      currentData = option.getAttribute("data-id");
+      dataName.innerText = `${data[currentData].label}:`;
+      title.innerText = data[currentData].title;
+      slider.min = data[currentData].yearRange.min;
+      slider.max = data[currentData].yearRange.max;
+      slider.value = data[currentData].yearRange.min;
+      sliderYear = slider.value;
+      year.innerText = slider.value;
       updateMap();
     });
   });
 }
 
 let currentColor = "red";
-let currentData = 1;
+let currentData = 0;
 
 const country = document.getElementById("country");
 const currData = document.getElementById("current-data");
@@ -543,20 +655,23 @@ const colorBtns = document.getElementsByClassName("color-radio");
 // define mappings for color, color scale, and metadata for the different data being displayed
 const colorKey = {red:"#ed3413", green:"#03ad36", blue:"#1a9cd9", purple:"#9803a6"};
 const scaleKey = {red: d3.interpolateYlOrRd, green: d3.interpolateBuGn, blue: d3.interpolateRdBu, purple: d3.interpolateMagma};
-const dataKey = {
-  1: {title: "Title for Data 1", units: "°F", label: "Data 1"},
-  2: {title: "Title for Data 2", units: "°C", label: "Data 2"},
-  3: {title: "Title for Data 3", units: "ppm", label: "Data 3"},
-  4: {title: "Title for Data 4", units: "in", label: "Data 4"}
-};
+let currCountry = "";
+// const dataKey = {
+//   1: {title: "Title for Data 1", units: "°F", label: "Data 1"},
+//   2: {title: "Title for Data 2", units: "°C", label: "Data 2"},
+//   3: {title: "Title for Data 3", units: "ppm", label: "Data 3"},
+//   4: {title: "Title for Data 4", units: "in", label: "Data 4"}
+// };
+
+// load and process data
+const data = await initData();
+console.log(data);
 
 // initialize side panel features
 initColorBtns();
 initMapSlider();
 initDropdown();
-
-// load and process data
-await initData();
+updateMap();
 
 // initialize the map, storing references for updating the map later
 const [path, g] = initMap();
